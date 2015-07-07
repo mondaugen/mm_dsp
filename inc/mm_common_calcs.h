3db94b87ec039e4f1b2a80bd841e14ac8624edd2
Nichoas Esterer <nicholas.esterer@gmail.com>
2015-07-07 16:50:13 -0400

    Added looking up of fractional powers of two for getting frequencies for tuning. This is untested in this implementation but comes from another project where it was working well.

--- inc/mm_common_calcs.h
+++ inc/mm_common_calcs.h
@@ -1,12 +1,14 @@
 #ifndef MM_COMMON_CALCS_H
 #define MM_COMMON_CALCS_H 
 
-#include <math.h> 
 #include "mm_sample.h"
 #include "mm_tuning.h"
 
+#define MMCC_ET12_TABLE_LENGTH 128 
+
 /* common conversions that need to be made */
-#define  MMCC_MIDItoRate(midinote) pow(2., (midinote - 69.) / 12.) \
-    * MMTuning_getA4Hz()
+#define MMCC_MIDItoRate(midinote) (MMCC_et12_rate(midinote) * MMTuning_getA4Hz())
+
+float MMCC_et12_rate(float pitch);
 
 #endif /* MM_COMMON_CALCS_H */
--- inc/mm_tuning.h
+++ inc/mm_tuning.h
@@ -1,7 +1,7 @@
 #ifndef MM_TUNING_H
 #define MM_TUNING_H 
 
-#define MM_TUNING_A4_HZ 440.
+#define MM_TUNING_A4_HZ 440.f
 
 #define MMTuning_getA4Hz() MM_TUNING_A4_HZ
 #endif /* MM_TUNING_H */
