6cd464bb617de4a6af8e932fd16575d32d36e32a
Nichoas Esterer <nicholas.esterer@gmail.com>
2017-01-18 11:15:27 -0500

    cubic interpolation with rate increment seems to pass tests.

--- test/build_interp_cubic_rinc_q_24_8_v_test.sh
+++ test/build_interp_cubic_rinc_q_24_8_v_test.sh
@@ -3,4 +3,4 @@ gcc interp_cubic_rinc_q_24_8_v_test.c \
 ../src/mm_interp.c \
 -I../inc \
 -o interp_cubic_rinc_q_24_8_v_test.bin \
--ggdb3
+-ggdb3 -lm
--- test/interp_cubic_rinc_q_24_8_v_test.c
+++ test/interp_cubic_rinc_q_24_8_v_test.c
@@ -1,5 +1,7 @@
+#include <stdint.h> 
 #include <assert.h>
 #include <stdio.h> 
+#include <math.h> 
 
 #include "mm_interp.h"
 
@@ -13,7 +15,8 @@
     }
 
 #define MM_TEST_ERR_TOL 1.e-6
-#define FABS(x) ((x < 0) ? (-x) : (x)) 
+#define MM_TEST_RINC_ERR_TOL 1.e-5
+#define FABS(x) (((x) < 0) ? (-(x)) : (x)) 
 
 static int test_correct_interp(void)
 {
@@ -32,25 +35,66 @@ static int test_correct_interp(void)
     return 1;
 }
 
+#define TLI_TAB_LEN 256
 static int test_loop_interp(void)
 {
-    float   in[] = {1.,2.,3.,-3.,-2.,-1.};
-    float   out[15];
-    int32_t idx0 = 3;
-    int32_t rate = (int32_t)(0.5 * 0x100);
+    float   in[TLI_TAB_LEN];
+    float   out[4*TLI_TAB_LEN];
+    int32_t idx0 = (TLI_TAB_LEN/2) << 8;
+    int32_t rate = (int32_t)(-0.5 * 0x100);
     int32_t rinc = 0;
-    // float   out_correct[] = {-3., 0, 3., 2.5, 2.,
-    //                          1.5, 1., .5, 0., -.5,
-    //                          -1., -1.5, -2., -2.5, -3.};
-    float   out_correct[] = {-3., 0, 3., 2.5, 2.,
-                             1.5, 1., .5, 0., -.5,
-                             -1., -1.5, -2., -2.5, -3.};
-    SHOW_TEST_START("test_loop_interp");
-    MM_interp_cubic_rinc_q_24_8_v(out,15,in,6,&idx0,&rate,rinc);
-    int n;
-    for (n = 0; n < 15; n++) {
-        assert(FABS(out[n] - out_correct[n]) < MM_TEST_ERR_TOL);
+    float   out_correct[4*TLI_TAB_LEN];
+    size_t n;
+    for (n = 0; n < TLI_TAB_LEN; n++) {
+        in[n] = sinf(((float)n)/TLI_TAB_LEN*2.*M_PI);
     }
+    for (n = 0; n < (4*TLI_TAB_LEN); n++) {
+        out_correct[n] = sinf((n*0.5)/TLI_TAB_LEN*2.*M_PI);
+    }
+    SHOW_TEST_START("test_loop_interp");
+    MM_interp_cubic_rinc_q_24_8_v(out,4*TLI_TAB_LEN,in,TLI_TAB_LEN,&idx0,&rate,rinc);
+    float maxerr = 0.;
+    for (n = 0; n < (4*TLI_TAB_LEN); n++) {
+        assert(FABS(out[n] - out_correct[n]) < MM_TEST_ERR_TOL);
+        maxerr = (maxerr < FABS(out[n] - out_correct[n])) ? FABS(out[n] - out_correct[n]) : maxerr;
+    }
+    printf("maxerr: %g ", maxerr);
+    return 1;
+}
+
+/* Test with rate increment */
+static int test_loop_interp_rinc(void)
+{
+    float   in[TLI_TAB_LEN];
+    float   out[4*TLI_TAB_LEN];
+    int32_t idx0 = 0;
+    int32_t rate = (int32_t)(1 * 0x100);
+    /* Will be going 5x as fast at the end of the interpolation
+     * NOTE: This will not work if the table length is more that 256. */
+    int32_t rinc = (4*0x100) / (4*TLI_TAB_LEN);
+    float   out_correct[4*TLI_TAB_LEN];
+    size_t n;
+    for (n = 0; n < TLI_TAB_LEN; n++) {
+        in[n] = sinf(((float)n)/TLI_TAB_LEN*2.*M_PI);
+    }
+    int32_t _rate = rate;
+    int32_t _idx = idx0;
+    for (n = 0; n < (4*TLI_TAB_LEN); n++) {
+        float tmp = (float)(_idx >> 8);
+        tmp += (float)(_idx & 0xff) / 256.f;
+        out_correct[n] = sinf(tmp/TLI_TAB_LEN*2.*M_PI);
+        _idx += _rate;
+        _rate += rinc;
+    }
+    SHOW_TEST_START("test_loop_interp_rinc");
+    MM_interp_cubic_rinc_q_24_8_v(out,4*TLI_TAB_LEN,in,TLI_TAB_LEN,&idx0,&rate,rinc);
+    assert(rate == _rate);
+    float maxerr = 0.;
+    for (n = 0; n < (4*TLI_TAB_LEN); n++) {
+        assert(FABS(out[n] - out_correct[n]) < MM_TEST_RINC_ERR_TOL);
+        maxerr = (maxerr < FABS(out[n] - out_correct[n])) ? FABS(out[n] - out_correct[n]) : maxerr;
+    }
+    printf("maxerr: %g ", maxerr);
     return 1;
 }
 
@@ -60,6 +104,8 @@ int main (void)
     rslt = test_correct_interp();
     SHOW_TEST_RESULT(rslt);
     rslt = rslt && test_loop_interp();
+    SHOW_TEST_RESULT(rslt);
+    rslt = rslt && test_loop_interp_rinc();
     SHOW_TEST_RESULT(rslt);
     return !rslt;
 }
